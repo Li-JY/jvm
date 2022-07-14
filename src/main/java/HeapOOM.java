b5cf7394ac3962dfd0cb7ccabdf3153cc0e88420
Hidden in the dark <ljyjunye@163.com>
2022-07-14 08:41:43 +0800

    Create HeapOOM.java

---
+++ src/main/java/HeapOOM.java
@@ -0,0 +1,22 @@
+/**
+ * -Xms20m -Xmx20m -XX:HeapDumpOutOfMemoryError
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapOOM {
+
+    static class OOMObject{
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+
+
+}
